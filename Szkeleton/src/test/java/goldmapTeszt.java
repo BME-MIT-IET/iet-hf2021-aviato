cc193f6873202d93804fddbc9acdf7d699c0e01d
DDorian13 <david.dorian99@gmail.com>
2021-05-16 15:00:35 +0200

    objektum fő funkciók

--- Szkeleton/src/test/java/goldmapTeszt.java
+++ Szkeleton/src/test/java/goldmapTeszt.java
@@ -53,4 +53,43 @@ public class goldmapTeszt {
         Palya.getAktJatekos().Atlep(Palya.getMezo("mezo9"));
         assertEquals("mezo9", Palya.getAktJatekos().getMezo().getId());
     }
+
+    // Teszt11
+    @Test
+    public void eves() {
+        assertEquals(5, Palya.getAktJatekos().getTestho());
+        assertEquals(4, Palya.getAktJatekos().getLepesszam());
+
+        Palya.getAktJatekos().Hasznal(Palya.getAktJatekos().getTargy(0), Palya.getAktJatekos().getMezo());
+
+        assertEquals(6, Palya.getAktJatekos().getTestho());
+        assertEquals(3, Palya.getAktJatekos().getLepesszam());
+    }
+
+    // Teszt12
+    @Test
+    public void menekites() {
+        Palya.getAktJatekos().Takarit();
+        Palya.getAktJatekos().Takarit();
+        Palya.getAktJatekos().Felvesz();
+        Palya.getAktJatekos().Atlep(Palya.getMezo("mezo13"));
+        Palya.getAktJatekos().Vegeztem();
+
+        Palya.getAktJatekos().Atlep(Palya.getMezo("mezo13"));
+        Palya.getAktJatekos().Atlep(Palya.getMezo("mezo7"));
+        Palya.getAktJatekos().Vegeztem();
+
+        Palya.getAktJatekos().Atlep(Palya.getMezo("mezo7"));
+        Palya.getAktJatekos().Vegeztem();
+
+        // medve
+        Palya.getAktJatekos().Atlep(Palya.getMezo("mezo1"));
+        Palya.getAktJatekos().Vegeztem();
+
+        assertEquals(2, Palya.getMezo("mezo7").getSzereplokSzama());
+        assertEquals(1, Palya.getMezo("mezo13").getSzereplokSzama());
+        Palya.getAktJatekos().Hasznal(Palya.getAktJatekos().getTargy(1), Palya.getMezo("mezo7"));
+        assertEquals(3, Palya.getMezo("mezo13").getSzereplokSzama());
+        assertEquals(0, Palya.getMezo("mezo7").getSzereplokSzama());
+    }
 }
